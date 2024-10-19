276b5803b90e6489525907fcca59274126fd4cae
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-19 11:42:16 -0300

    Task Store Request

--- laravel11_vue3_inertia/task_table_project/app/Http/Controllers/Api/version1/TaskController.php
+++ laravel11_vue3_inertia/task_table_project/app/Http/Controllers/Api/version1/TaskController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api\version1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
+use Illuminate\Contracts\Cache\Store;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,6 +15,13 @@ class TaskController extends Controller
     {
         // return Task::all();
         return TaskResource::collection(Task::all());
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        $task = Task::create($request->validated());
+
+        return TaskResource::make($task);
     }
 
     public function show(Task $task)
--- laravel11_vue3_inertia/task_table_project/app/Http/Requests/StoreTaskRequest.php
+++ laravel11_vue3_inertia/task_table_project/app/Http/Requests/StoreTaskRequest.php
@@ -4,14 +4,14 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class Task extends FormRequest
+class StoreTaskRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class Task extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
         ];
     }
 }
