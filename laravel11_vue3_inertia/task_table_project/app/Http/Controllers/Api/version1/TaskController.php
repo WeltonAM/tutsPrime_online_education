3042a8fb35d5789ffe8e856aca5de216b4e64a3a
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-19 11:36:44 -0300

    Task Resource

--- laravel11_vue3_inertia/task_table_project/app/Http/Controllers/Api/version1/TaskController.php
+++ laravel11_vue3_inertia/task_table_project/app/Http/Controllers/Api/version1/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\version1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,12 @@ class TaskController extends Controller
 {
     public function index()
     {
-        return Task::all();
+        // return Task::all();
+        return TaskResource::collection(Task::all());
+    }
+
+    public function show(Task $task)
+    {
+        return TaskResource::make($task);
     }
 }
