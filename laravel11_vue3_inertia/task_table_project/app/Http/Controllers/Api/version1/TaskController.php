018a010f8b5c28b6d6d44d2e45040a7e07dd4531
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-19 11:30:32 -0300

    Task API route

---
+++ laravel11_vue3_inertia/task_table_project/app/Http/Controllers/Api/version1/TaskController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\version1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::all();
+    }
+}
