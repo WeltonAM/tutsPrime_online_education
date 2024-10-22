14acb9ab6491a8fc303d0a4e7b2f59b2a14c35f6
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-22 08:17:10 -0300

    Summary endpoint api

--- laravel11_vue3_inertia/task_table_project/routes/api/v2.php
+++ laravel11_vue3_inertia/task_table_project/routes/api/v2.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\version2\CompleteTaskController;
+use App\Http\Controllers\Api\version2\SummaryController;
 use App\Http\Controllers\Api\version2\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->prefix('v2')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
+    Route::get('/summaries', SummaryController::class);
 });
