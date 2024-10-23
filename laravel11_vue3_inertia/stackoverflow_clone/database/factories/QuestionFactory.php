ead96b421f8f8c1948a578d9ddd4c2bf9310bee2
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-23 11:24:44 -0300

    Question factory and seed

---
+++ laravel11_vue3_inertia/stackoverflow_clone/database/factories/QuestionFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(1),
+            'body' => $this->faker->text(100),
+            'slug' => $this->faker->unique()->slug(),
+            'views' => rand(0, 10),
+            'answers' => rand(0, 10),
+            'votes' => rand(-3, 10),
+        ];
+    }
+}
